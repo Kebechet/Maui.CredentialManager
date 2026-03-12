420ba95df42f8945747ee3218b80f47ac8cd67d7
Kebechet <samosidor@gmail.com>
2026-03-12 13:34:00 +0100

    make demo usable

--- demo/DemoApp/MauiProgram.cs
+++ demo/DemoApp/MauiProgram.cs
@@ -1,28 +1,37 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using Maui.CredentialManagers.Extensions;
+using Microsoft.Extensions.Logging;
 
 namespace DemoApp;
 
 public static class MauiProgram
 {
-	public static MauiApp CreateMauiApp()
-	{
-		var builder = MauiApp.CreateBuilder();
-		builder
-			.UseMauiApp<App>()
-			.ConfigureFonts(fonts =>
-			{
-				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-			});
+    public static MauiApp CreateMauiApp()
+    {
+        var builder = MauiApp.CreateBuilder();
+        builder
+            .UseMauiApp<App>()
+            .ConfigureFonts(fonts =>
+            {
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+            });
 
-		builder.Services.AddMauiBlazorWebView();
+        builder.Services.AddMauiBlazorWebView();
 
-		// TODO: Register Maui.CredentialManagers services here
+        builder.Services.AddCredentialManagerService(options =>
+        {
+            // Configure SSO client IDs here when testing SSO flows
+            // options.GoogleServerClientId = "your-google-client-id";
+            // options.GoogleIosClientId = "your-google-ios-client-id";
+            // options.GoogleIosRedirectUri = "your-redirect-uri";
+            // options.AppleServiceId = "your-apple-service-id";
+            // options.AppleRedirectUri = "your-apple-redirect-uri";
+        });
 
 #if DEBUG
-		builder.Services.AddBlazorWebViewDeveloperTools();
-		builder.Logging.AddDebug();
+        builder.Services.AddBlazorWebViewDeveloperTools();
+        builder.Logging.AddDebug();
 #endif
 
-		return builder.Build();
-	}
+        return builder.Build();
+    }
 }
